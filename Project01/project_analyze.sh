8a7c3d2d920dc5123b1989281f666855e140822b
Eugene Fedoriv <fedorivy@mcmaster.ca>
2020-02-12 05:47:46 +0000

    Checkout Latest Merge

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -3,13 +3,17 @@
 path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$path"
 
+if [ "$1" -eq 1 ] ; then
+
+
 dir=$(find ../ -type f | grep -v .git)
+
 
 if [ -f fixme.log ]; then 
 	rm fixme.log
 fi
-
 touch fixme.log
+
 
 for f in $dir
 do
@@ -17,3 +21,23 @@ do
 		echo $f >> fixme.log
 	fi
 done
+
+
+elif [ "$1" -eq 2 ] ; then
+
+IFS=$'\n'
+for c in $(git log --oneline)
+do
+	if [ $(echo "$c" | egrep -i "*merge") ]; then	
+	commit="${c%% *}"
+	git checkout "$commit" 
+	break
+	fi
+
+done
+
+fi
+
+
+#Replacing all the occurrence of the pattern in a line : The substitute flag /g (global replacement) specifies the sed command to replace all the occurrences of the string in the line.
+#$sed 's/unix/linux/g' geekfile.txt
